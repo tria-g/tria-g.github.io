3602605d7852e5ccaa9d3293585d319ea9a2feea
tria <gonzalestria@yahoo.com>
2020-06-15 21:37:36 +0800

    edits

--- js/custom.js
+++ js/custom.js
@@ -4,8 +4,8 @@ $(window).load(function () {
     "use strict";
     // makes sure the whole site is loaded
     $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({
+    $('#preloader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(150).css({
         'overflow': 'visible'
     });
 })
